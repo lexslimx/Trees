821bd15270a1857f7bfe36f794df675cdd9638cf
alex <alex.km@live.com>
2021-12-19 16:52:43 +0300

    added issametree

---
+++ IsomorphicTrees.cs
@@ -0,0 +1,11 @@
+public class IsomorphicTrees{
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p !=null && q == null) return false;
+        if(p ==null && q != null) return false;
+        
+        if(p.val != q.val) return false;
+        
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+}
