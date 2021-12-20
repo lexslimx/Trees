ea893f2002e538209b07724a93742d945cd5ac52
alex <alex.km@live.com>
2021-12-20 22:02:58 +0300

    added max path sum

---
+++ MaximumPathSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    internal class MaximumPathSum
+    {
+        private int max = int.MinValue;
+        public int MaxPathSum(TreeNode root)
+        {
+            PathSum(root);
+            return max;
+        }
+
+        private int PathSum(TreeNode node)
+        {
+            if (node == null) return 0;
+            int leftSum = Math.Max(0, PathSum(node.Left));
+            int rightSum = Math.Max(0, PathSum(node.Right));
+            int currentSum = node.val + leftSum + rightSum;
+            max = Math.Max(max, currentSum);
+
+            return Math.Max(leftSum, rightSum) + node.val;
+        }
+    }
+}
